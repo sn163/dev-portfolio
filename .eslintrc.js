57894b5cd4831f4e02a79d4b6d04204101e6408f
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-08-25 00:21:58 -0700

    changed json to js

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+      },
+    },
+  ],
+};
