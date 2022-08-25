f6c4d3af8b9cb692a7d53a7a596e9f2017426a72
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-08-25 00:14:08 -0700

    updated lints and configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './assets/App.scss';
-import './assets/theme.scss';
-import About from './components/About.jsx';
-import Blog from './components/Blog.jsx';
-import Contact from './components/Contact.jsx';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import logo from "./logo.svg";
+import "./assets/App.scss";
+import "./assets/theme.scss";
+import About from "./components/About.jsx";
+import Blog from "./components/Blog.jsx";
+import Contact from "./components/Contact.jsx";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
