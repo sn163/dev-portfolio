0ca0eaf1c1d524fcf15e4ae18742f9518ff09f4a
Sonny Nguyen <sonnynguyen163@gmail.com>
2023-09-06 23:14:21 -0700

    test

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -23,7 +23,7 @@ export default function NavHeader() {
               <Nav.Link>Projects</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/contact">
-              <Nav.Link>Contact Test</Nav.Link>
+              <Nav.Link>Contact test 2</Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
