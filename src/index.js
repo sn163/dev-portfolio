49606e3e07676483e2e857fcc1686e3b685c08d3
Sonny Nguyen <sonnynguyen163@gmail.com>
2023-10-17 21:27:34 -0700

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.scss";
 import "./index.css";
+import "./App.scss";
 import App from "./App.tsx";
 import reportWebVitals from "./reportWebVitals.js";
 import { ThemeProvider } from "react-bootstrap";
@@ -18,7 +18,7 @@ root.render(
         <App />
       </HashRouter>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
