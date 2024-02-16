d2c28f0e61c58c32a9309330ad61695d6d661e12
Sonny Nguyen <sonnynguyen163@gmail.com>
2024-02-15 23:05:11 -0800

    move plugins back to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
-import plugins from "./plugins";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: "/",
-    plugins: plugins(mode),
-    build: {
-      outDir: "build",
-    },
-    server: {
-      // this ensures that the browser opens upon server start
-      open: true,
-      // this sets a default port to 3000
-      port: 3000,
-    },
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgr({
+      include: "**/*.svg?react",
+      exclude: "",
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
 });
